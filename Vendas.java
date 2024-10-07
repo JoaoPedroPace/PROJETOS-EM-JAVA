5f05f52a79cc02fc145135b88dd8d9edeeda3af1
JoaoPedroPace <jpedrotorres791@gmail.com>
2024-10-07 15:06:51 -0300

    Add files via upload

---
+++ Vendas.java
@@ -0,0 +1,43 @@
+
+package com.mycompany.controledevendascombancodedados;
+
+public class Vendas {
+    private String nome_cliente;
+    private String nome_produto;
+    private int quantidade;
+    private float preco_produto;
+
+    public String getNome_cliente() {
+        return nome_cliente;
+    }
+
+    public void setNome_cliente(String nome_cliente) {
+        this.nome_cliente = nome_cliente;
+    }
+
+    public String getNome_produto() {
+        return nome_produto;
+    }
+
+    public void setNome_produto(String nome_produto) {
+        this.nome_produto = nome_produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public float getPreco_produto() {
+        return preco_produto;
+    }
+
+    public void setPreco_produto(float preco_produto) {
+        this.preco_produto = preco_produto;
+    }
+    
+    
+}
